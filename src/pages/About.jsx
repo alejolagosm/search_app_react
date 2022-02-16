bfac8bb1d809e63015a1ab42b9b95b52581a793e
alejolagosm <falame94@hotmail.com>
2022-02-16 16:59:51 -0500

    First commit

---
+++ src/pages/About.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+function About() {
+  return (
+    <div>
+      <h1 className="text-4xl mb-4">Github Finder</h1>
+      <p className="mb-4 text-xl font-light">
+        A React APP to search Github profiles and the details of a given
+        project. The original design an idea for the project was from
+        <a href="https://www.traversymedia.com/">Brad Traversy </a>
+      </p>
+    </div>
+  );
+}
+
+export default About;
